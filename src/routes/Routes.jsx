e444dcdbc5a8b533721878d05231aef2f119dd58
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-11-05 08:15:28 +0600

    initialize setup done with preline component library

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+
+
+const Routes = createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout/>
+    }
+])
+
+export default Routes;
