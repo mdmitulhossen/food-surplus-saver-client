cc792296ffec876d2d7e67843a2984c655673e87
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-11-06 16:22:22 +0600

    all food available section done

--- src/Pages/AvailableFooodPage.jsx
+++ src/Pages/AvailableFooodPage.jsx
@@ -1,25 +1,51 @@
+import { useForm } from "react-hook-form";
 import Adds from "../components/Adds";
 import Breadcrumb from "../components/Breadcrumb";
 import FoodCard from "../components/card/FoodCard";
 
 const AvailableFooodPage = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm()
+    const onSubmit = (data) => console.log(data)
     return (
         <div >
             <Adds />
             <div className="foodContainer py-10">
                 <Breadcrumb />
 
-                <div className="mt-10 border rounded-md p-4">
+                <div className="mt-10 border rounded-md p-2 pt-8">
                     <h1 className="text-4xl font-bold text-center text-[#0C4428] ">All Available Foods</h1>
 
 
-                    <div className="grid grid-cols-12 mt-10">
+                    <div className="grid grid-cols-12 mt-10 gap-2">
 
-                        <div className="md:col-span-3">
-
+                        <div className="md:col-span-3 space-y-5">
+                            <div>
+                                <p className="text-xl font-bold mb-2">Search</p>
+                                <input
+                                    {...register("search")}
+                                    type="text" placeholder="serach by food name"
+                                    className="px-1 py-2 border border-[#8DC53E] rounded-md w-full"
+                                />
+                            </div>
+                            <div>
+                                <p className="text-xl font-bold mb-2">Sort</p>
+                                <select name="" id="" {...register("sort")} className="px-1 py-2 border border-[#8DC53E] rounded-md w-full">
+                                    <option value="" selected disabled>Sort</option>
+                                    <option value="expireDate">Expire Date</option>
+                                </select>
+                                {/* <input
+                                    {...register("search")}
+                                    type="text" placeholder="serach by food name"
+                                    className="px-1 py-2 border rounded-md w-full"
+                                /> */}
+                            </div>
                         </div>
                         {/* right */}
-                        <div className="md:col-span-9 grid grid-cols-3 gap-4">
+                        <div className="md:col-span-9 grid grid-cols-3 gap-2">
                             <FoodCard />
                             <FoodCard />
                             <FoodCard />
