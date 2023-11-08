b550bbe0aabdf2cc467aafcaa2d862ed569b549e
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-11-08 22:09:55 +0600

    navbar mobile menu problem solve

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -6,15 +6,17 @@ import toast from 'react-hot-toast';
 const Navbar = () => {
   const location = useLocation()
   const [userOpen, setUserOpen] = useState(false)
-  const { user,logOut } = useAuth() || {};
+  const [openMobileMenu, setOpenMobileMenu] = useState(false)
+
+  const { user, logOut } = useAuth() || {};
 
   console.log(user)
 
-  
-  const handleLogOut = ()=>{
+
+  const handleLogOut = () => {
     logOut()
-    .then(result => toast.success('successfully Logout'))
-    .catch(err => toast.error(err))
+      .then(result => toast.success('successfully Logout'))
+      .catch(err => toast.error(err))
     setUserOpen(false)
   }
   const menu = [
@@ -26,7 +28,7 @@ const Navbar = () => {
 
   ]
   return (
-    <div className={`${location.pathname==='/' ? 'absolute ':'bg-[#0C4428]' }  top-0 w-full z-50`}>
+    <div className={`${location.pathname === '/' ? 'absolute ' : 'bg-[#0C4428]'}  top-0 w-full z-50`}>
       <header className=" z-50 w-full bg-transparent  py-3 lg:py-0 dark:bg-gray-800 dark:border-gray-700">
         <nav className="relative foodContainer w-full lg:flex lg:items-center lg:justify-between " aria-label="Global">
           <div className="flex items-center justify-between">
@@ -35,7 +37,7 @@ const Navbar = () => {
               <img src={logo} className='w-[35px] h-[35px] object-fill ' alt="" />
               <Link className="flex-none text-lg sm:text-2xl lg:text-xl xl:text-2xl font-bold dark:text-white text-white" href="#" aria-label="Brand">Food-<span className='text-[#c0ffc5]'>Surplus</span>-Saver</Link>
             </div>
-          {/* user menu login mobile menu*/}
+            {/* user menu login mobile menu*/}
             <div className='flex gap-5 lg:hidden'>
               <div>
                 {
@@ -52,7 +54,7 @@ const Navbar = () => {
                     </div>
                   </div>
                     :
-                    <NavLink  to='/login' className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-[#64AF5D] lg:border-l sm:border-gray-300 lg:my-6 lg:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-[#618264]">
+                    <NavLink to='/login' className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-[#64AF5D] lg:border-l sm:border-gray-300 lg:my-6 lg:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-[#618264]">
                       <button type="button" className="flex items-center justify-center gap-2 w-full px-4 py-2 text-sm font-medium text-black hover:text-white bg-[#c0ffc5] rounded-md hover:bg-[#64AF5D] transition-all">
                         <span className="flex items-center gap-1">
                           <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -67,11 +69,11 @@ const Navbar = () => {
 
 
               <div className="">
-                <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-[#c0ffc5] text-black hover:text-white shadow-sm align-middle hover:bg-[#618264]/80 focus:outline-none   text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
-                  <svg className="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <button onClick={()=>setOpenMobileMenu(!openMobileMenu)} type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-[#c0ffc5] text-black hover:text-white shadow-sm align-middle hover:bg-[#618264]/80 focus:outline-none   text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
+                  <svg className={` ${openMobileMenu?'hidden':'block'} w-4 h-4`} width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                   </svg>
-                  <svg className="hs-collapse-open:block hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <svg className={`${openMobileMenu?'block':'hidden'}  w-4 h-4`} width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                   </svg>
                 </button>
@@ -80,7 +82,7 @@ const Navbar = () => {
           </div>
 
           {/* menu */}
-          <div id="navbar-collapse-with-animation" className="hs-collapse hidden transition-all duration-200 basis-full grow lg:block bg-[#c0ffc5] rounded lg:rounded-none  p-5 mt-5 lg:bg-transparent lg:p-0 lg:mt-0">
+          <div id="navbar-collapse-with-animation" className={`${openMobileMenu?'block':'hidden'} transition-all duration-200 basis-full grow lg:block bg-[#c0ffc5] rounded lg:rounded-none  p-5 mt-5 lg:bg-transparent lg:p-0 lg:mt-0`}>
             <div className="flex flex-col gap-y-4 gap-x-0  lg:flex-row lg:items-center lg:justify-end lg:gap-y-0 lg:gap-x-3 xl:gap-x-7 lg:mt-0 lg:pl-2 xl:pl-7 pr-3">
               {
                 menu.map((item, index) => <NavLink
